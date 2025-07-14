64d3b228804b7cd4c1c2c5eb27ac3ad8e666662f
Dallas Huff <46586333+DallasHuff@users.noreply.github.com>
2025-07-14 09:27:59 -0500

    Update ui_sound.gd

--- common/autoloads/ui_sound.gd
+++ common/autoloads/ui_sound.gd
@@ -1,19 +1,33 @@
 extends Node2D
 
+var button_press_sounds: Array[AudioStreamPlayer] = []
+var button_hover_sounds: Array[AudioStreamPlayer] = []
+
+@onready var press_1: AudioStreamPlayer = $ButtonSound
+@onready var hover_1: AudioStreamPlayer = $ButtonHoverSound
+
+
 func _ready() -> void:
 	# when _ready is called, there might already be nodes in the tree, so connect all existing buttons
 	connect_buttons(get_tree().root)
-	get_tree().node_added.connect(_on_SceneTree_node_added)
+	get_tree().node_added.connect(_on_tree_node_added)
 
-func _on_SceneTree_node_added(node: Node) -> void:
+	button_press_sounds = [press_1]
+	button_hover_sounds = 
+
+
+func _on_tree_node_added(node: Node) -> void:
 	if node is Button:
 		connect_to_button(node)
 
-func _on_Button_pressed() -> void:
-	$ButtonSound.play()
-	
-func _on_Mouse_entered() -> void:
-	$ButtonHoverSound.play()
+
+func _on_button_pressed() -> void:
+	button_press_sounds.pick_random().play()
+
+
+func _on_mouse_entered() -> void:
+	button_hover_sounds.pick_random().play()
+
 
 # recursively connect all buttons
 func connect_buttons(root: Node) -> void:
@@ -22,6 +36,7 @@ func connect_buttons(root: Node) -> void:
 			connect_to_button(child)
 		connect_buttons(child)
 
+
 func connect_to_button(button: Button) -> void:
-	button.pressed.connect(_on_Button_pressed)
-	button.mouse_entered.connect(_on_Mouse_entered)
+	button.pressed.connect(_on_button_pressed)
+	button.mouse_entered.connect(_on_mouse_entered)
