e4277c3f29e62c62c19d178ecea19b2979edf286
DallasHuff <dallashuff45@gmail.com>
2025-07-14 21:18:15 -0500

    added health potions

--- combat_area/currency_mover.gd
+++ combat_area/currency_mover.gd
@@ -12,9 +12,8 @@ func _ready() -> void:
 	CombatEvents.enemy_died.connect(_on_enemy_died)
 	
 func _on_enemy_died(enemy: Enemy) -> void:
-	var target_pos: Vector2 = Vector2(266.0, 120.0)
 	var cur_instance: Node2D
-	var bloods: int = bloods = enemy.stats.blood
+	var bloods: int = enemy.stats.blood
 	var souls: int = enemy.stats.souls
 	var memories: int = enemy.stats.memories
 	if enemy.stats.blood >= 0:
@@ -37,4 +36,3 @@ func _on_enemy_died(enemy: Enemy) -> void:
 	tween.tween_callback(CombatEvents.blood_gained.emit.bind(bloods))
 	tween.tween_callback(CombatEvents.souls_gained.emit.bind(souls))
 	tween.tween_callback(CombatEvents.memories_gained.emit.bind(memories))
-	
