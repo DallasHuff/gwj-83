e72885a4bea3701574fb39ae9a7ed3af66e5a46e
Huff13 <kylehuff13@gmail.com>
2025-07-20 10:19:17 -0500

    sfx

--- shop/shop.gd
+++ shop/shop.gd
@@ -2,9 +2,9 @@ class_name Shop
 extends Control
 
 @export var attack_damage_upgrade: Upgrade
-var blood: int = 3
-var souls: int = 1
-var memories: int = 1
+var blood: int = 200
+var souls: int = 200
+var memories: int = 200
 var upgrade_containers: Array[UpgradeContainer] = []
 
 @onready var blood_count_label: Label = %BloodCountLabel
